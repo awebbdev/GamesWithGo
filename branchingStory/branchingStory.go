7d4c2f1f2f8afcb2f8f20a349ed854cdc7386d74
thebigbadox <adamwebb13@gmail.com>
2018-06-28 12:34:49 -0500

    Completed Branchjing story demo

--- branchingStory/branchingStory.go
+++ branchingStory/branchingStory.go
@@ -1,35 +1,48 @@
 package main
 
 import (
-	"os"
 	"bufio"
 	"fmt"
+	"os"
 )
 
-type storyNode struct{
-	text string
-	yesNode *storyNode
-	noNode *storyNode
+type storyNode struct {
+	text    string
+	yesPath *storyNode
+	noPath  *storyNode
 }
 
-func (node *storyNode) play(){
+func (node *storyNode) printStory(depth int) {
+	for i := 0; i < depth*2; i++{
+		fmt.Print("  ")
+	}
+	fmt.Print(node.text + "\n")
+	if node.yesPath != nil{
+		node.yesPath.printStory(depth + 1)
+	}
+	if node.noPath != nil{
+		node.noPath.printStory(depth + 1)
+	}	
+}
+
+func (node *storyNode) play() {
 	fmt.Println(node.text)
-	if node.yesNode != nil && node.noNode != nil {
+	if node.yesPath != nil && node.noPath != nil {
 		scanner := bufio.NewScanner(os.Stdin)
 		for {
 			scanner.Scan()
 			answer := scanner.Text()
-			if answer == "yes"{
-				node.yesNode.play()
+			if answer == "yes" {
+				node.yesPath.play()
 				break
-			}else if answer == "no"{
-				node.noNode.play()
+			} else if answer == "no" {
+				node.noPath.play()
 				break
-			}else{
+			} else {
 				fmt.Println("Incorrect input")
 			}
 		}
-	}	
+	}
 }
 
 func main() {
@@ -37,8 +50,8 @@ func main() {
 	root := storyNode{"You are at the entrance to a dark cave. Do you want to go in the cave?", nil, nil}
 	winning := storyNode{"You have won!", nil, nil}
 	losing := storyNode{"You have lost!", nil, nil}
-	root.noNode = &winning
-	root.yesNode = &losing
+	root.noPath = &winning
+	root.yesPath = &losing
 
-	root.play()
+	root.printStory(0)
 }
