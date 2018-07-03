7e000aea73c3053b9a46f205f75cf64c3a8201e2
thebigbadox <adamwebb13@gmail.com>
2018-07-02 21:08:24 -0500

    Added color and output of values

--- simplexnoise/simplexnoise.go
+++ simplexnoise/simplexnoise.go
@@ -6,28 +6,81 @@ import (
 )
 
 const winWidth, winHeight int = 800, 600
-func rescaleAndDraw(noise []float32,min, max float32, pixels []byte){
+
+func lerp(b1, b2 byte, pct float32) byte {
+	return byte(float32(b1) + pct*(float32(b2)-float32( b1)))
+}
+
+func colorLerp(c1,c2 color, pct float32) color {
+	return color{lerp(c1.r, c2.r, pct), lerp(c1.g, c2.g, pct), lerp(c1.b, c2.b, pct)}
+}
+
+func getGradient(c1, c2 color) []color {
+	result := make( []color, 256)
+	for i := range result {
+		pct := float32(i) / float32(255)
+		result[i] = colorLerp(c1, c2, pct)
+	}
+	return result
+}
+
+func getDualGradient(c1, c2, c3, c4 color) []color {
+	result := make( []color, 256)
+	for i := range result {
+		pct := float32(i) / float32(255)
+		if pct < 0.5 {			
+			result[i] = colorLerp(c1, c2, pct*float32(2))
+		} else {
+			result[i] = colorLerp(c3, c4, pct*float32(1.5)- float32(0.5))
+		}
+	}
+	return result
+}
+
+func clamp(min, max, v int) int {
+	if v < min {
+		v = min
+	}else if v > max {
+		v = max
+	}
+	return v
+}
+
+func rescaleAndDraw(noise []float32,min, max float32, gradient []color, pixels []byte){
 	scale := 255.0 / (max - min)
 	offset := min * scale
 
 	for i := range noise {
 		noise[i] = noise[i]*scale - offset
-		b := byte(noise[i])
-		pixels[i*4] = b
-		pixels[i *4+1] = b
-		pixels[i*4+2] = b
+		c := gradient[clamp(0,2255, int(noise[i]))]
+		p := i*4
+		pixels[p] = c.r
+		pixels[p+1] = c.g
+		pixels[p+2] = c.b
 	}
 
 }
 
-func makeNoise(pixels []byte) {
+func fbm2(x, y, frequency, lacunarity, gain float32, octaves int) float32{
+	var sum float32
+	amplitude := float32(1.0)
+	for i := 0; i < octaves; i++{
+		sum += snoise2(x*frequency, y*frequency)*amplitude
+		frequency = frequency * lacunarity
+		amplitude = amplitude * gain
+	}
+	return sum
+}
+
+func makeNoise(pixels []byte, frequency, lacunarity, gain float32, octaves int) {
 	noise := make([]float32, winWidth*winHeight)
+	fmt.Printf("freq: %v, lac: %v, gain: %v, octaves: %v\n", frequency, lacunarity, gain, octaves)
 	i:= 0
 	min := float32(9999.0)
 	max := float32(-9999.0)
 	for y := 0; y < winHeight; y++ {
 		for x:= 0; x < winWidth; x++{
-			noise[i] = (snoise2(float32(x)/100.0, float32(y)/100.0))
+			noise[i] = fbm2(float32(x), float32(y), frequency, lacunarity, gain, octaves)
 			if noise[i] < min {
 				min = noise[i]
 			}else if noise[i] > max {
@@ -36,7 +89,8 @@ func makeNoise(pixels []byte) {
 			i++
 		}
 	}
-	rescaleAndDraw(noise, min, max, pixels)
+	gradient := getDualGradient(color{0, 0, 175}, color{80, 160, 244}, color{12, 192, 75}, color{255, 255, 255})
+	rescaleAndDraw(noise, min, max, gradient, pixels)
 }
 
 type color struct {
@@ -78,12 +132,51 @@ func main(){
 	defer tex.Destroy()
 
 	pixels := make([]byte, winWidth*winHeight*4)
+	frequency := float32(0.01)
+	octaves := 3
+	gain := float32(0.2)
+	lacunarity := float32(3.0)
 
-	makeNoise(pixels)
+	makeNoise(pixels, frequency, lacunarity, gain, octaves)
+	keyState := sdl.GetKeyboardState()
 
-	tex.Update(nil, pixels, winWidth * 4)
-	renderer.Copy(tex, nil, nil)
-	renderer.Present()
+
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				return
+			}
+		}
+		
+		mult := 1
+		if keyState[sdl.SCANCODE_LSHIFT] != 0 || keyState[sdl.SCANCODE_RSHIFT] != 0 {
+			mult = -1
+		}
+		if keyState[sdl.SCANCODE_O] != 0 {
+			octaves = octaves + 1*mult
+			makeNoise(pixels, frequency, lacunarity, gain, octaves)
+		}
+		if keyState[sdl.SCANCODE_F] != 0 {
+			frequency = frequency + .001*float32(mult)
+			makeNoise(pixels, frequency, lacunarity, gain, octaves)
+		}
+		if keyState[sdl.SCANCODE_G] != 0 {
+			gain = gain + 0.1*float32(mult)
+			makeNoise(pixels, frequency, lacunarity, gain, octaves)
+		}
+		if keyState[sdl.SCANCODE_L] != 0 {
+			lacunarity = lacunarity + 0.1*float32(mult)
+			makeNoise(pixels, frequency, lacunarity, gain, octaves)
+		}
+
+
+		tex.Update(nil, pixels, winWidth * 4)
+		renderer.Copy(tex, nil, nil)
+		renderer.Present()
+
+		sdl.Delay(16)
+	}
 }
 
 
