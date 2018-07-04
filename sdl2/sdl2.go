269f830f532c4f22d770142aea148d8c8d110ed1
thebigbadox <adamwebb13@gmail.com>
2018-07-04 11:14:36 -0500

    Added balloons to demo alpha blending

--- sdl2/sdl2.go
+++ sdl2/sdl2.go
@@ -7,11 +7,11 @@ import (
 
 const winWidth, winHeight int = 800, 600
 
-type color struct {
+type rgba struct {
 	r, g, b byte
 }
 
-func setPixel(x, y int, c color, pixels []byte) {
+func setPixel(x, y int, c rgba, pixels []byte) {
 	index := (y * winWidth + x) * 4
 
 	if index < len(pixels) - 4 && index >= 0{
@@ -20,6 +20,7 @@ func setPixel(x, y int, c color, pixels []byte) {
 		pixels[index + 2] = c.b
 	}
 }
+
 
 func main(){
 
@@ -47,15 +48,16 @@ func main(){
 
 	pixels := make([]byte, winWidth*winHeight*4)
 
-	for y := 0; y < winHeight ; y++ {
-		for x := 0 ; x < winWidth ; x++ {
-			setPixel(x, y, color{byte(x % 255),byte(y % 255), 0}, pixels)
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent(){
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				return
+			}
 		}
+		tex.Update(nil, pixels, winWidth * 4)
+		renderer.Copy(tex, nil, nil)
+		renderer.Present()
+		sdl.Delay(16)
 	}
-	tex.Update(nil, pixels, winWidth * 4)
-	renderer.Copy(tex, nil, nil)
-	renderer.Present()
-
-	sdl.Delay(2000)
-
 }
