f06a68b0e193f4826d4381cfeba739d630143ff0
thebigbadox <adamwebb13@gmail.com>
2018-07-12 08:48:00 -0500

    Added more math funchtions and demoed them in the main app

--- evolvingpictures/apt/apt.go
+++ evolvingpictures/apt/apt.go
@@ -6,7 +6,9 @@ package apt
 //Double Node (+, -)
 
 import (
+	"strconv"
 	"math"
+	"github.com/awebbdev/gameswithgo/noise"
 )
 
 type Node interface {
@@ -37,6 +39,41 @@ func (op *OpSin) String() string {
 	return "( Sin " + op.Child.String() + " )"
 }
 
+type OpCos struct {
+	SingleNode
+}
+
+func (op *OpCos) Eval(x,y float32) float32 {
+	return float32(math.Cos(float64(op.Child.Eval(x,y))))
+}
+
+func (op *OpCos) String() string {
+	return "( Cos " + op.Child.String() + " )"
+}
+
+type OpAtan struct {
+	SingleNode
+}
+
+func (op *OpAtan) Eval(x,y float32) float32 {
+	return float32(math.Atan(float64(op.Child.Eval(x,y))))
+}
+
+func (op *OpAtan) String() string {
+	return "( Atan " + op.Child.String() + " )"
+}
+
+type OpNoise struct {
+	DoubleNode
+}
+
+func (op *OpNoise) Eval(x,y float32) float32 {
+	return 80*noise.Snoise2(op.LeftChild.Eval(x,y), op.RightChild.Eval(x,y)) -2.0
+}
+
+func (op *OpNoise) String() string {
+	return " ( SimplexNoise " + op.LeftChild.String() + " " + op.RightChild.String() + " )"
+}
 type OpPlus struct {
 	DoubleNode
 }
@@ -49,6 +86,53 @@ func (op *OpPlus) String() string {
 	return "( + " +op.LeftChild.String() + " " + op.RightChild.String() + " )"
 }
 
+type OpMinus struct {
+	DoubleNode
+}
+
+func (op *OpMinus) Eval(x,y float32) float32 {
+	return op.LeftChild.Eval(x,y) - op.RightChild.Eval(x,y)
+}
+
+func (op *OpMinus) String() string {
+	return "( - " +op.LeftChild.String() + " " + op.RightChild.String() + " )"
+}
+
+type OpMult struct {
+	DoubleNode
+}
+
+func (op *OpMult) Eval(x,y float32) float32 {
+	return op.LeftChild.Eval(x,y) * op.RightChild.Eval(x,y)
+}
+
+func (op *OpMult) String() string {
+	return "( * " +op.LeftChild.String() + " " + op.RightChild.String() + " )"
+}
+
+type OpDiv struct {
+	DoubleNode
+}
+
+func (op *OpDiv) Eval(x,y float32) float32 {
+	return op.LeftChild.Eval(x,y) / op.RightChild.Eval(x,y)
+}
+
+func (op *OpDiv) String() string {
+	return "( / " +op.LeftChild.String() + " " + op.RightChild.String() + " )"
+}
+
+type OpAtan2 struct {
+	DoubleNode
+}
+
+func (op *OpAtan2) Eval(x,y float32) float32 {
+	return float32(math.Atan2(float64(y), float64(x)))
+}
+
+func (op *OpAtan2) String() string {
+	return "( Atan2 " +op.LeftChild.String() + " " + op.RightChild.String() + " )"
+}
 type OpX LeafNode
 
 func (op *OpX) Eval(x,y float32) float32 {
@@ -67,4 +151,17 @@ func (op *OpY) Eval(x,y float32) float32 {
 
 func (op *OpY) String() string {
 	return "Y"
+}
+
+type OpConstant struct { 
+	LeafNode 
+	value float32
+}
+
+func (op *OpConstant) Eval(x,y float32) float32 {
+	return op.value
+}
+
+func (op *OpConstant) String() string{
+	return strconv.FormatFloat(float64(op.value), 'f', 9, 32)
 }
--- evolvingpictures/evolvingpictures.go
+++ evolvingpictures/evolvingpictures.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"github.com/veandco/go-sdl2/sdl"
 	"fmt"
-	"time"
 	. "github.com/awebbdev/gameswithgo/evolvingpictures/apt"
+	"github.com/veandco/go-sdl2/sdl"
+	"time"
 )
 
 const winWidth, winHeight, winDepth int = 800, 600, 100
 
 type audioState struct {
-	explosionBytes	[]byte
-	deviceID		sdl.AudioDeviceID
-	audiosSpec		*sdl.AudioSpec
+	explosionBytes []byte
+	deviceID       sdl.AudioDeviceID
+	audiosSpec     *sdl.AudioSpec
 }
 
 type mouseState struct {
-	leftButton	bool
-	rightButton	bool
-	x,y			int
+	leftButton  bool
+	rightButton bool
+	x, y        int
 }
 
 func getMouseState() mouseState {
@@ -32,6 +32,7 @@ func getMouseState() mouseState {
 	result.rightButton = !(rightButton == 0)
 	return result
 }
+
 type rgba struct {
 	r, g, b byte
 }
@@ -47,17 +48,17 @@ func clear(pixels []byte) {
 }
 
 func setPixel(x, y int, c rgba, pixels []byte) {
-	index := (y * winWidth + x) * 4
+	index := (y*winWidth + x) * 4
 
-	if index < len(pixels) - 4 && index >= 0{
+	if index < len(pixels)-4 && index >= 0 {
 		pixels[index] = c.r
-		pixels[index + 1] = c.g
-		pixels[index + 2] = c.b
+		pixels[index+1] = c.g
+		pixels[index+2] = c.b
 	}
 }
 
-func pixelsToTexture(renderer *sdl.Renderer, pixels []byte, w,h int) *sdl.Texture {
-	tex,err := renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, int32(w), int32(h))
+func pixelsToTexture(renderer *sdl.Renderer, pixels []byte, w, h int) *sdl.Texture {
+	tex, err := renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, int32(w), int32(h))
 	if err != nil {
 		panic(err)
 	}
@@ -65,25 +66,26 @@ func pixelsToTexture(renderer *sdl.Renderer, pixels []byte, w,h int) *sdl.Textur
 	return tex
 }
 
-func APTToTexture (node1, node2 Node, w, h int, renderer *sdl.Renderer) *sdl.Texture {
+func aptToTexture(redNode, greenNode, blueNode Node, w, h int, renderer *sdl.Renderer) *sdl.Texture {
 	//-1.0 and 1.0
-	scale := float32(255/2)
-	offset := float32(-1.0*scale)
-	pixels := make([]byte,w*h*4)
+	scale := float32(255 / 2)
+	offset := float32(-1.0 * scale)
+	pixels := make([]byte, w*h*4)
 	pixelIndex := 0
 	for yi := 0; yi < h; yi++ {
 		y := float32(yi)/float32(h)*2 - 1
 		for xi := 0; xi < w; xi++ {
 			x := float32(xi)/float32(w)*2 - 1
 
-			c := node1.Eval(x,y)
-			c2 := node2.Eval(x,y)
+			r := redNode.Eval(x, y)
+			g := greenNode.Eval(x, y)
+			b := blueNode.Eval(x, y)
 
-			pixels[pixelIndex] = byte(c*scale - offset)
+			pixels[pixelIndex] = byte(r*scale - offset)
 			pixelIndex++
-			pixels[pixelIndex] = byte(c2*scale - offset)
+			pixels[pixelIndex] = byte(g*scale - offset)
 			pixelIndex++
-			pixels[pixelIndex] = 0 //byte(c*scale - offset)
+			pixels[pixelIndex] = byte(b*scale - offset)
 			pixelIndex++
 			pixelIndex++
 
@@ -92,10 +94,10 @@ func APTToTexture (node1, node2 Node, w, h int, renderer *sdl.Renderer) *sdl.Tex
 	return pixelsToTexture(renderer, pixels, w, h)
 }
 
-func main(){
+func main() {
 	sdl.LogSetAllPriority(sdl.LOG_PRIORITY_VERBOSE)
 	window, err := sdl.CreateWindow("Evolving Pictures", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-	int32(winWidth), int32(winHeight), sdl.WINDOW_SHOWN)
+		int32(winWidth), int32(winHeight), sdl.WINDOW_SHOWN)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -109,7 +111,7 @@ func main(){
 	}
 	defer renderer.Destroy()
 
-/*  	explosionBytes, audioSpec := sdl.LoadWAV("explode.wav")
+	/*  	explosionBytes, audioSpec := sdl.LoadWAV("explode.wav")
 	audioID, err := sdl.OpenAudioDevice("", false, audioSpec, nil, 0)
 	if err != nil {
 		panic(err)
@@ -119,7 +121,7 @@ func main(){
 	audioState := audioState{explosionBytes, audioID, audioSpec} */
 
 	sdl.SetHint(sdl.HINT_RENDER_SCALE_QUALITY, "1")
-	
+
 	var elapsedTime float32
 	var currentMouseState = getMouseState()
 	//var previousMouseState = currentMouseState
@@ -127,18 +129,25 @@ func main(){
 	x := &OpX{}
 	y := &OpY{}
 	sine := &OpSin{}
+	noise := &OpNoise{}
+	atan2 := &OpMult{}
 	plus := &OpPlus{}
+	atan2.LeftChild = x
+	atan2.RightChild = noise
+	noise.LeftChild = x
+	noise.RightChild = y
+	sine.Child = atan2
+	plus.LeftChild = y
+	plus.RightChild = sine
 
-	sine.Child = x
-	plus.LeftChild = sine
-	plus.RightChild = y
 
-	tex := APTToTexture(plus, sine, 800, 600, renderer)
+
+	tex := aptToTexture(plus, plus, plus, 800, 600, renderer)
 
 	for {
 		frameStart := time.Now()
 
-		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent(){
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch e := event.(type) {
 			case *sdl.QuitEvent:
 				return
@@ -151,11 +160,10 @@ func main(){
 			}
 		}
 		currentMouseState = getMouseState()
-		
+
 		renderer.Copy(tex, nil, nil)
 		renderer.Present()
 		elapsedTime = float32(time.Since(frameStart).Seconds() * 1000)
-		//fmt.Println("ms per frame: ", elapsedTime)
 		if elapsedTime < 5 {
 			sdl.Delay(5 - uint32(elapsedTime))
 			elapsedTime = float32(time.Since(frameStart).Seconds() * 1000)
