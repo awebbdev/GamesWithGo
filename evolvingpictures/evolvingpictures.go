2acda9d3918975405deff1955798712d809cd5df
thebigbadox <adamwebb13@gmail.com>
2018-07-12 06:25:34 -0500

    Implemented texture drawing using APT

--- evolvingpictures/evolvingpictures.go
+++ evolvingpictures/evolvingpictures.go
@@ -1,19 +1,164 @@
 package main
 
 import (
+	"github.com/veandco/go-sdl2/sdl"
 	"fmt"
+	"time"
 	. "github.com/awebbdev/gameswithgo/evolvingpictures/apt"
 )
 
-func main() {
+const winWidth, winHeight, winDepth int = 800, 600, 100
+
+type audioState struct {
+	explosionBytes	[]byte
+	deviceID		sdl.AudioDeviceID
+	audiosSpec		*sdl.AudioSpec
+}
+
+type mouseState struct {
+	leftButton	bool
+	rightButton	bool
+	x,y			int
+}
+
+func getMouseState() mouseState {
+	mouseX, mouseY, mouseButtonState := sdl.GetMouseState()
+	leftButton := mouseButtonState & sdl.ButtonLMask()
+	rightButton := mouseButtonState & sdl.ButtonRMask()
+	var result mouseState
+	result.x = int(mouseX)
+	result.y = int(mouseY)
+	result.leftButton = !(leftButton == 0)
+	result.rightButton = !(rightButton == 0)
+	return result
+}
+type rgba struct {
+	r, g, b byte
+}
+
+type pos struct {
+	x, y float32
+}
+
+func clear(pixels []byte) {
+	for i := range pixels {
+		pixels[i] = 0
+	}
+}
+
+func setPixel(x, y int, c rgba, pixels []byte) {
+	index := (y * winWidth + x) * 4
+
+	if index < len(pixels) - 4 && index >= 0{
+		pixels[index] = c.r
+		pixels[index + 1] = c.g
+		pixels[index + 2] = c.b
+	}
+}
+
+func pixelsToTexture(renderer *sdl.Renderer, pixels []byte, w,h int) *sdl.Texture {
+	tex,err := renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, int32(w), int32(h))
+	if err != nil {
+		panic(err)
+	}
+	tex.Update(nil, pixels, w*4)
+	return tex
+}
+
+func APTToTexture (node Node, w, h int, renderer *sdl.Renderer) *sdl.Texture {
+	//-1.0 and 1.0
+	scale := float32(255/2)
+	offset := float32(-1.0*scale)
+	pixels := make([]byte,w*h*4)
+	pixelIndex := 0
+	for yi := 0; yi < h; yi++ {
+		y := float32(yi)/float32(h)*2 - 1
+		for xi := 0; xi < w; xi++ {
+			x := float32(xi)/float32(w)*2 - 1
+
+			c := node.Eval(x,y)
+
+			pixels[pixelIndex] = byte(c*scale - offset)
+			pixelIndex++
+			pixels[pixelIndex] = byte(c*scale - offset)
+			pixelIndex++
+			pixels[pixelIndex] = byte(c*scale - offset)
+			pixelIndex++
+			pixelIndex++
+
+		}
+	}
+	return pixelsToTexture(renderer, pixels, w, h)
+}
+
+func main(){
+	sdl.LogSetAllPriority(sdl.LOG_PRIORITY_VERBOSE)
+	window, err := sdl.CreateWindow("Evolving Pictures", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+	int32(winWidth), int32(winHeight), sdl.WINDOW_SHOWN)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer renderer.Destroy()
+
+/*  	explosionBytes, audioSpec := sdl.LoadWAV("explode.wav")
+	audioID, err := sdl.OpenAudioDevice("", false, audioSpec, nil, 0)
+	if err != nil {
+		panic(err)
+	}
+	defer sdl.FreeWAV(explosionBytes)
+
+	audioState := audioState{explosionBytes, audioID, audioSpec} */
+
+	sdl.SetHint(sdl.HINT_RENDER_SCALE_QUALITY, "1")
+	
+	var elapsedTime float32
+	var currentMouseState = getMouseState()
+	//var previousMouseState = currentMouseState
+
 	x := &OpX{}
 	y := &OpY{}
-	plus := &OpPlus{}
 	sine := &OpSin{}
+	plus := &OpPlus{}
+
 	sine.Child = x
 	plus.LeftChild = sine
 	plus.RightChild = y
 
-	fmt.Println(plus.Eval(5, 2))
-	fmt.Println(plus)
+	tex := APTToTexture(plus, 800, 600, renderer)
+
+	for {
+		frameStart := time.Now()
+
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent(){
+			switch e := event.(type) {
+			case *sdl.QuitEvent:
+				return
+			case *sdl.TouchFingerEvent:
+				touchX := e.X * float32(winWidth)
+				touchY := e.Y * float32(winHeight)
+				currentMouseState.x = int(touchX)
+				currentMouseState.y = int(touchY)
+				currentMouseState.leftButton = true
+			}
+		}
+		currentMouseState = getMouseState()
+		
+		renderer.Copy(tex, nil, nil)
+		renderer.Present()
+		elapsedTime = float32(time.Since(frameStart).Seconds() * 1000)
+		//fmt.Println("ms per frame: ", elapsedTime)
+		if elapsedTime < 5 {
+			sdl.Delay(5 - uint32(elapsedTime))
+			elapsedTime = float32(time.Since(frameStart).Seconds() * 1000)
+		}
+		//previousMouseState = currentMouseState
+	}
 }
